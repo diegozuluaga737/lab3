fc75c948212e883354a15a1444c43862b26de463
Diego Zuluaga <diego.zuluaga1@udea.edu.co>
2020-10-22 20:21:13 -0500

    caso 1 con string

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -4,51 +4,83 @@
 //Primer metodo.....
 
 int main(){
-//-----------------------codificacion------------------------------------//
-   string nombre;
-   cout<<"ingrese el nombre del archivo a codificar"<<endl;
-   cin>>nombre;
-
-  string texto=lectura(nombre);
-  string strbinario=binario(texto);
-
-
-  int n;
-  cout<<"ingrese la semilla de codificacion"<<endl;
-  cin>>n;
-
- string bincod= metodo(n, strbinario);
- //string cod=codificado(bincod);
-string cod=Binary_to_Str( bincod);
- cout<<"ingrese el nombre que tendra el archivo codificado"<<endl;
- cin>>nombre;
-
-
- archivobinario(cod, nombre);
-
- //-------------------------------decodificacion------------------------------//
-
-
- cout<<"ingrese el nombre del archivo a decodificar"<<endl;
- cin>>nombre;
 
 
 
- texto=lectura(nombre);
- strbinario=binario(texto);
-
-cout<<"ingrese la semilla de decodificacion"<<endl;
-cin>>n;
-
-bincod=metododecodificacion(n,strbinario);
-//cod=codificado(bincod);
-cod=Binary_to_Str( bincod);
-cout<<"ingrese el nombre que tendra el archivo decodificado"<<endl;
-cin>>nombre;
 
 
-archivobinario(cod, nombre);
+    int met;
+    int dec;
+    cout<<"ingrese 1.para el metodo numero uno, 2. para el metodo numero 2"<<endl;
+    cin>>met;
+
+    switch (met) {
+
+    case 1: {
+        cout<<"ingrese 1.para codificar, 2. para decodificar"<<endl;
+        cin>>dec;
+
+        switch (dec) {
+
+        case 1: {
+            string nombre;
+            cout<<"ingrese el nombre del archivo a codificar"<<endl;
+            cin>>nombre;
+
+           string texto=lectura(nombre);
+           string strbinario=binario(texto);
+
+
+           int n;
+           cout<<"ingrese la semilla de codificacion"<<endl;
+           cin>>n;
+
+          string bincod= metodo(n, strbinario);
+         string cod=Binary_to_Str( bincod);
+          cout<<"ingrese el nombre que tendra el archivo codificado"<<endl;
+          cin>>nombre;
+
+
+          archivobinario(cod, nombre);
+
+            break;
+        }
+        case 2:{
+            string nombre;
+            cout<<"ingrese el nombre del archivo a decodificar"<<endl;
+            cin>>nombre;
+
+
+
+          string    texto=lectura(nombre);
+          string  strbinario=binario(texto);
+
+
+          int n;
+           cout<<"ingrese la semilla de decodificacion"<<endl;
+           cin>>n;
+
+          string bincod=metododecodificacion(n,strbinario);
+          string cod=Binary_to_Str( bincod);
+           cout<<"ingrese el nombre que tendra el archivo decodificado"<<endl;
+           cin>>nombre;
+
+
+           archivobinario(cod, nombre);
+
+            break;
+             }
+
+        }
+
+        break;
+    }
+    case 2:{
+
+        break;
+    }
+
+    }
 
  return 0;
 }
-//---
